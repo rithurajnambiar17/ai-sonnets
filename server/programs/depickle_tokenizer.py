771dc17dd984965f4e0e57e6e495bea6ab46f1d6
Rithuraj Nambiar <69073735+rithurajnambiar17@users.noreply.github.com>
2023-01-07 13:05:46 +0530

    small changes

--- server/programs/depickle_tokenizer.py
+++ server/programs/depickle_tokenizer.py
@@ -2,5 +2,5 @@ import pickle
 
 def depickle_tokenizer(path):
     with open(path, 'rb') as handle:
-        tokenizerLoaded=pickle.load(handle)
+        tokenizerLoaded = pickle.load(handle)
     return tokenizerLoaded
